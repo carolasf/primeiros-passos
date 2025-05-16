e3f2199b33c0c4746deec35133412a323a7fbcee
Carolina Saraiva Ferreira <75094303+carolasf@users.noreply.github.com>
2025-05-16 15:56:38 -0300

    Renomeia login.spec.cy.js para user.spec.cy.js

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,17 +8,34 @@ describe('Orange HRM Tests', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
     dashboardGrid: '.orangehrm-dashboard-grid',
-    wrongCredentialAlert: '.oxd-alert'
+    wrongCredentialAlert: '.oxd-alert',
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
   }
 
-  it('Login - Success', () => {
+  it.only('User - Success', () => {
+
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('Employee')
+    //cy.get(selectorsList.genericField).eq(4).type('Employee')
+    //cy.get(selectorsList.genericField).eq(5).type('OtherIdTest')
+    //cy.get(selectorsList.genericField).eq(6).type('DriverLicenseNumberTest')
+    //cy.get(selectorsList.genericField).eq(7).type('2025-03-10')
+    //cy.get(selectorsList.genericField).eq(8).type('SSNNumberTest')
+
   })
+
   it('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
