5bd0ea866a4797e9249ba51b3b6a66fb87964944
Carolina Saraiva Ferreira <75094303+carolasf@users.noreply.github.com>
2025-05-18 21:51:25 -0300

    Challenge finished

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -12,8 +12,11 @@ describe('Orange HRM Tests', () => {
     myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
+    genericField: '.oxd-input--active',
     dateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: '.--close',
+    genericComboBox: '.oxd-select-text--arrow',
+    submitButton: "[type='submit']",
   }
 
   it.only('User - Success', () => {
@@ -28,11 +31,20 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
     cy.get(selectorsList.genericField).eq(3).clear().type('Employee')
-    //cy.get(selectorsList.genericField).eq(4).type('Employee')
-    //cy.get(selectorsList.genericField).eq(5).type('OtherIdTest')
-    //cy.get(selectorsList.genericField).eq(6).type('DriverLicenseNumberTest')
-    //cy.get(selectorsList.genericField).eq(7).type('2025-03-10')
-    //cy.get(selectorsList.genericField).eq(8).type('SSNNumberTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('DriverLicenseTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2025-03-10')
+    cy.get(selectorsList. dateCloseButton).click()
+    /*cy.get(selectorsList.genericComboBox).eq(0).click()
+    cy.get(selectorsList.genericComboBox).eq(0).should('be.visible')
+    cy.get('.oxd-select-dropdown').contains('Algerian').click()*/
+    cy.get(selectorsList.genericComboBox).eq(1).click()
+    cy.get(selectorsList.genericComboBox).eq(1).should('be.visible')
+    cy.get('.oxd-select-dropdown').contains('Single').click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
+
 
   })
 
